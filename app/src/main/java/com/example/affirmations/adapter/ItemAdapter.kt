093f430d96f981732d1a2fe73e37551fd82a98de
Pujitha Pilla <poojasuhana8@gmail.com>
2021-04-05 23:15:30 +0530

    Added files

---
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.affirmations.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.R
+import com.example.affirmations.model.Affirmation
+
+class ItemAdapter(val context: Context, val dataset: List<Affirmation>): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.txt_title)
+        val imgView: ImageView = view.findViewById(R.id.img_title)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context).inflate(R.layout.list_item,parent,false)
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imgView.setImageResource(item.drawableResourceId)
+    }
+
+    override fun getItemCount(): Int {
+        return dataset.size
+    }
+}
